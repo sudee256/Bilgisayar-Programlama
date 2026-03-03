a2e471f3a44cc5face6c7144d9acc1fcbe11b54f
Sude Şahin <ssude699@gmail.com>
2026-03-03 23:05:48 +0300

    Add files via upload

---
+++ Bilgisayar Programlama/Ödev 2/Ödev 2.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main() {
+   
+    int num1,num2;
+    
+    printf("Enter first integer\n ");
+    scanf("%d" , &num1);
+     printf("Enter second integer\n ");
+    scanf("%d" , &num2);
+    
+    int sum = num1 + num2;
+    
+    printf("Sum is: %d\n" , sum);
+    
+    return 0 ;
+}
